429556e36272cf0f4718f611306998e736f98902
Ted Ip   <hip7@my.bcit.ca>
2025-02-07 14:24:37 -0800

    Establishing basic structure of the project

---
+++ src/BookStore.java
@@ -0,0 +1,12 @@
+
+/**
+ * The BookStore
+ * class has a main() method which creates a BookStore object. The BookStore contains a second instance
+ * variable that is an List of Novel references; the BookStore constructor populates that ArrayList with the
+ * following data:(a big table of Data)
+* */
+
+
+
+public class BookStore {
+}
---
+++ src/Novel.java
@@ -0,0 +1,9 @@
+/**
+ * Novel class with private instance variables, constructor parameters, and accessor methods for
+ * “title”, “author name”, and “year published”
+ * */
+
+
+
+public class Novel {
+}
